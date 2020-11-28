bf101117840657d6abf7eb780a231855953e7dd7
Harindu Liyanage <harindu.sul@gmail.com>
2020-11-28 20:03:34 +0530

    configure module federation plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 
-const HtmlWebPackPlugin = require('html-webpack-plugin')
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
 
 module.exports = {
     mode: "development",
@@ -7,6 +8,12 @@ module.exports = {
         port: 3000,
     },
     plugins: [
+        new ModuleFederationPlugin({
+            name: 'container',
+            remotes: {
+                kassandra: 'kassandra@http://localhost:3001/remoteEntry.js'
+            }
+        }),
         new HtmlWebPackPlugin({
             template: "./public/index.html"
         })
